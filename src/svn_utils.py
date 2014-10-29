43e5b3294f2b20dcbc8b6e8dfc50d49db90d37dc
Povilas Balciunas <balciunas90@gmail.com>
2014-10-29 17:06:40 +0200

    Added svn utilities module.

---
+++ src/svn_utils.py
@@ -0,0 +1,36 @@
+#
+# This module has utility functions related to svn repo manipulation.
+#
+
+import os
+from exceptions import Exception
+
+#
+# Returns svn repository base path.
+# Throws exception if current directory is not within svn repository.
+#
+def find_svn_base_dir():
+	cur_dir = os.getcwd()
+	files = os.listdir(cur_dir)
+
+	while keep_looking_for_svn_dir(files, cur_dir):
+		os.chdir("..")
+		cur_dir = os.getcwd()
+		files = os.listdir(cur_dir)
+
+	if not has_svn_dir(files):
+		raise Exception("This directory is not part of svn repository.")
+
+	return cur_dir
+
+
+def keep_looking_for_svn_dir(files, cur_dir):
+	return (not has_svn_dir(files)) and (not is_base_path(os.getcwd()))
+
+
+def has_svn_dir(files_dirs):
+	return ".svn" in files_dirs
+
+
+def is_base_path(path):
+	return path == "/"
