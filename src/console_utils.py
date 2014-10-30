05bb259cf3138e59a23fc531380f7e8b32f90536
Povilas Balciunas <balciunas90@gmail.com>
2014-10-30 12:03:30 +0200

    Added functions to colorize text.

--- src/console_utils.py
+++ src/console_utils.py
@@ -3,8 +3,17 @@
 #
 
 COLOR_RED = "\033[31m"
+COLOR_GREEN = "\033[32m"
 COLOR_RESET = "\033[0m"
 
 
 def print_error(err_msg):
 	print COLOR_RED + err_msg + COLOR_RESET
+
+
+def text_green(text):
+	return COLOR_GREEN + text + COLOR_RESET
+
+
+def text_red(text):
+	return COLOR_RED + text + COLOR_RESET
